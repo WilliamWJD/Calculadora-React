9124bd46223ba67c51f15c14091b4c7b57406c37
William José Dias <william.jose.wjd@gmail.com>
2019-08-23 08:57:48 -0300

    Implementando a logica

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,10 +4,20 @@ import './Calculator.css'
 import Button from '../Components/Button'
 import Display from '../Components/Display'
 
+const initialState={
+    displayValue:'0',
+    clearDisplay:false,
+    operation:null,
+    values:[0,0],
+    current:0
+}
+
 export default class Calculator extends Component{
     
+    state={...initialState}
+
     clearMemory(){
-        console.log('limpar')
+        this.setState({...initialState})
     }
 
     setOperation(operation){
@@ -15,7 +25,24 @@ export default class Calculator extends Component{
     }
 
     addDigit(n){
-        console.log(n)
+        if(n==='.'&&this.state.displayValue.includes('.')){
+            return
+        }
+
+        const clearDisplay=this.state.displayValue==='0'
+            || this.state.clearDisplay
+        const currentValue=clearDisplay?'':this.state.displayValue
+        const displayValue=currentValue+n
+        this.setState({displayValue, clearDisplay:false})
+
+        if(n !== '.'){
+            const i=this.state.current
+            const  newValue=parseFloat(displayValue)
+            const values=[...this.state.values]
+            values[i]=newValue
+            this.setState({values})
+            console.log(values)
+        }
     }
 
     render(){
@@ -23,7 +50,7 @@ export default class Calculator extends Component{
         const setOperation=op=>this.setOperation(op)
         return(
             <div className="calculator">
-                <Display value={100}/>
+                <Display value={this.state.displayValue}/>
                 
                 <Button label="AC" click={()=>this.clearMemory()} triple/>
                 <Button label="/" click={setOperation} operation/>
